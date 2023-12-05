b11fa017c5f16daefe842854a7409ee6ebbfad98
Kaarlo <kaarlocaballeronillukka@gmail.com>
2023-12-05 10:03:06 +0000

    File prepared to work

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# cap = cv2.VideoCapture(1)
 
 import cv2
 
-# Load the cascade
-# face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')    # face detector
-
-cap = cv2.VideoCapture("people_walking.mp4")    #
-# cap = cv2.VideoCapture(1)
+body_detector = cv2.CascadeClassifier('haarcascade_fullbody.xml')  # load face detector
+video = cv2.VideoCapture("people_walking.mp4")  # Creates an object of opencv to be able to work with the video
 
 while (1):
-    retVal, frame = cap.read()  # Take each frame
-    if not retVal:  # if there's still frames
+    retVal, frame = video.read()  # Take each frame
+    if not retVal:  # if there's not any frame exit the loop
         break
-    frame = cv2.resize(frame, (400, 300)) # Resize image
-
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convertir la imagen a grises
-    faces = body_cascade.detectMultiScale(frame)    # Detect bodies
+    frame = cv2.resize(frame, (400, 300))  # Resize image
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert color image to black and white
+    faces = body_detector.detectMultiScale(frame)  # Detect bodies
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)    # pinta rectangulo
-
-    cv2.imshow('frame', frame)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Draw rectangles
+    cv2.imshow('frame', frame)  # Shows the video in a new window
 
 ##### do not touch this ##############################################################
-    k = cv2.waitKey(5) & 0xFF   #
+    k = cv2.waitKey(5) & 0xFF  # escape button to exit the video
     if k == 27:
         break
-cv2.destroyAllWindows()
-cap.release()
+cv2.destroyAllWindows()     # closes all the windows opened with imshow
+video.release()         # releases all the resources used in the video processing
 ######################################################################################
-
-class people:
-
-
