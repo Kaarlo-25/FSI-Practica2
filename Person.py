c56269a5bafcbc5bdcf720696fef7b8e8072857e
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-01-03 21:35:27 +0000

    Restructured code and error in roi and template size

--- Person.py
+++ Person.py
@@ -1,5 +1,12 @@
+import cv2
+
+
 class Person:
-    def __init__(self, x_center, y_center, x_coor, y_coor, width, height, color, trajectory=(0, 0)):
+    ID = 0
+    people = []
+
+    # Constructor
+    def __init__(self, center, x_coor, y_coor, width, height, color, template):
         """
         Constructor of class Person.
 
@@ -12,16 +19,15 @@ class Person:
         - height (int): Height of the rectangle
         - trajectory(tuple): Trajectory of the person
         """
-        self.center = [x_center, y_center]
+        Person.ID += 1
+        self.ID = Person.ID
+        self.center = center
         self.x_coor = x_coor
         self.y_coor = y_coor
         self.width = width
         self.height = height
         self.color = color
-        self.trajectory = trajectory
-
-    def __str__(self):
-        return f"[color:{self.color}, center:{self.center}, coor:[{self.x_coor}, {self.y_coor}], width:{self.width}, height:{self.height}]"
+        self.template = template
 
     def get_center(self):
         return self.center
@@ -29,5 +35,53 @@ class Person:
     def get_color(self):
         return self.color
 
-    def get_trajectory(self):
-        return self.trajectory
+    def set_center(self, new_center):
+        self.center = new_center
+
+    def set_x_coor(self, new_x_coor):
+        self.center = new_x_coor
+
+    def set_y_coor(self, new_y_coor):
+        self.center = new_y_coor
+
+    def set_template(self, new_template):
+        self.center = new_template
+
+    def set_width(self, new_width):
+        self.center = new_width
+
+    def set_height(self, new_height):
+        self.center = new_height
+
+    def __str__(self):
+        return f"[ID:{self.ID}, color:{self.color}, center:{self.center}, coor:[{self.x_coor}, {self.y_coor}], width:{self.width}, height:{self.height}]"
+
+    @classmethod
+    def same_person(cls, template, frame):
+        # min(Person.people, key=lambda person: (abs(person.get_center()[1] - center[1])))
+        """
+        Objetivo: conseguir la persona del frame anterior
+        Requisitos: template matching
+        - for por cada persona de people:
+            - coges el centro de la persona
+            - haces la region de interes y el template matching en el frame actual
+            - la mayor coincidencia es la persona
+        """
+
+        for person in Person.people:
+            x = int(person.center[0] - 25)
+            width = 60
+            y = int(person.center[1] + 25)
+            height = 60
+            roi = frame[y:(y + height), x:(x + width)]
+            cv2.imshow('ROI', roi)
+            cv2.imshow('template', person.template)
+            template_comparison = cv2.matchTemplate(roi, person.template, cv2.TM_CCOEFF_NORMED)
+            a, max_value, b, max_loc = cv2.minMaxLoc(template_comparison)
+            if max_value > 0.9:
+                person.x_coor = max_loc[0]
+                person.y_coor = max_loc[1]
+                person.template = template
+                person.center = [(person.width/2) + max_loc[0], (person.height/2) + max_loc[1]]
+                return Person.people.index(person)
+        return None
