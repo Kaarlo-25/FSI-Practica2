2e64e6701a7b87140f12ba20947b49fda1b59110
JoseLuisPerdomo <145450270+JoseLuisPerdomo@users.noreply.github.com>
2024-01-05 15:54:24 +0000

    Detection almost done (TODO: Delete person)

--- Person.py
+++ Person.py
@@ -6,7 +6,7 @@ class Person:
     people = []
 
     # Constructor
-    def __init__(self, center, x_coor, y_coor, width, height, color, template):
+    def __init__(self, center, x_coor, y_coor, width, height, color, template, is_update=[False, 2]):
         """
         Constructor of class Person.
 
@@ -28,6 +28,7 @@ class Person:
         self.height = height
         self.color = color
         self.template = template
+        self.is_update = is_update
 
     def get_center(self):
         return self.center
@@ -53,11 +54,18 @@ class Person:
     def set_height(self, new_height):
         self.center = new_height
 
+    def update_coord(self, new_x_coor, new_y_coor, new_center):
+        self.x_coor = new_x_coor
+        self.y_coor = new_y_coor
+        self.center = new_center
+        self.is_update[0] = True
+        self.is_update[1] = 3
+
     def __str__(self):
         return f"[ID:{self.ID}, color:{self.color}, center:{self.center}, coor:[{self.x_coor}, {self.y_coor}], width:{self.width}, height:{self.height}]"
 
     @classmethod
-    def same_person(cls, template, frame):
+    def same_person(cls, template, frame, center, x, y):
         # min(Person.people, key=lambda person: (abs(person.get_center()[1] - center[1])))
         """
         Objetivo: conseguir la persona del frame anterior
@@ -67,31 +75,44 @@ class Person:
             - haces la region de interes y el template matching en el frame actual
             - la mayor coincidencia es la persona
         """
-
+        close_centers = []
         for person in Person.people:
-            x = int(person.center[0])
-            width = 200
-            y = int(person.center[1])
-            height = 200
-            x_start = max(0, x - width // 2)
-            y_start = max(0, y - height // 2)
-            x_end = min(frame.shape[1], x + width // 2)
-            y_end = min(frame.shape[0], y + height // 2)
-            roi = frame[y_start:y_end, x_start:x_end]
-            """
-            x = int(person.center[0] - 25)
-            width = 80
-            y = int(person.center[1] + 25)
-            height = 80
-            roi = frame[y:(y + height), x:(x + width)]
-            """
-            template_comparison = cv2.matchTemplate(roi, person.template, cv2.TM_CCORR_NORMED)
-            min_value, max_value, min_loc, max_loc = cv2.minMaxLoc(template_comparison)
-            cv2.imshow("ROI", cv2.resize(roi, (400, 300)))
-            cv2.imshow("Template", cv2.resize(template, (200, 500)))
-            if max_value > 0.90:
-                person.x_coor = max_loc[0]
-                person.y_coor = max_loc[1]
-                #person.template = template
-                person.center = [(person.width/2) + max_loc[0], (person.height/2) + max_loc[1]]
-                return Person.people.index(person)
+            if (abs(person.center[0] - center[0]) < 6 and abs(person.center[1] - center[1]) < 6) or (
+                    abs(person.x_coor - x) < 15 and abs(person.y_coor - y) < 15):
+                close_centers.append(person)
+            if person.y_coor < 1 or person.y_coor + person.height == 599:
+                Person.people.remove(person)
+
+        if len(close_centers) == 0:
+            return None
+        if len(close_centers) == 1:
+            close_centers[0].update_coord(x, y, center)
+            return close_centers[0]
+        else:
+            person_found = min(close_centers, key=lambda person: (abs(person.get_center()[1] - center[1])))
+            return person_found
+
+
+    """
+        else:
+            for person in Person.people:
+                center_x = int(center[0])
+                width = 200
+                center_y = int(center[1])
+                height = 200
+                x_start = max(0, center_x - width // 2)
+                y_start = max(0, center_y - height // 2)
+                x_end = min(frame.shape[1], x + width // 2)
+                y_end = min(frame.shape[0], y + height // 2)
+                roi = frame[y_start:y_end, x_start:x_end]
+                template_comparison = cv2.matchTemplate(roi, person.template, cv2.TM_SQDIFF_NORMED)
+                min_value, max_value, min_loc, max_loc = cv2.minMaxLoc(template_comparison)
+                cv2.imshow("ROI", cv2.resize(roi, (400, 300)))
+                cv2.imshow("Template", cv2.resize(person.template, (400, 300)))
+                if max_value > 0.90:
+                    person.x_coor = x
+                    person.y_coor = y
+                    person.template = template
+                    person.center = [(person.width / 2) + x, (person.height / 2) + y]
+                    return Person.people.index(person)
+    """
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import random
 from Person import Person
 import cv2
 
-
-body_detector = cv2.CascadeClassifier('full_body_detector.xml')  # load body detector
-video = cv2.VideoCapture("people_walking.mp4")  # Creates an object of opencv to be able to work with the video
+body_detector = cv2.CascadeClassifier('full_body_detector.xml')
+video = cv2.VideoCapture("people_walking.mp4")
 
 i = 0
 while True:
@@ -25,7 +24,7 @@ while True:
         center = None
         template = None
 
-        if 10 < width < 65 and 25 < height < 120:
+        if 20 < width < 55 and 35 < height < 100:
             center = [(width / 2) + x, (height / 2) + y]
             template = frame[y:y + height, x:x + width]
             if i == 0:
@@ -34,16 +33,27 @@ while True:
                 ID = "Mierda"
 
             else:
-                coincidence = Person.same_person(template, frame)
+                coincidence = Person.same_person(template, frame, center, x, y)
                 if coincidence is not None:
-                    color = Person.people[coincidence].color
-                    ID = Person.people[coincidence].ID
+                    color = coincidence.color
+                    ID = coincidence.ID
                 else:
                     color = (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
                     Person.people.append(Person(center, x, y, width, height, color, template))
+                    ID = Person.people[-1].ID
             rectangle = cv2.rectangle(rectangle_frame, (x, y), (x + width, y + height), color, 2)
-            cv2.putText(rectangle_frame, f"{ID}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(rectangle_frame, f"{ID}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.imshow('frame', rectangle_frame)  # Shows the video in a new window
+
+    #for person in Person.people:
+
+        #person.is_update[0] == False and person.is_update[1] == 0
+
+        #if person.y_coor < 1 or person.y_coor + person.height == 599:
+            #Person.people.remove(person)
+        #if not person.is_update[0]:
+            #person.is_update[1] -= 1
+
     i += 1
     # escape button to exit the video
     k = cv2.waitKey(5) & 0xFF
